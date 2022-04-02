b95ccfb67b3c729a46f6f50e7230125263c59a28
berdogan <berkanterdogan92@gmail.com>
2022-04-02 23:15:46 +0300

    first commit. the project was initialized

---
+++ src/main/java/com/berkanterdogan/lab/springaoplab/SpringAopLabApplication.java
@@ -0,0 +1,13 @@
+package com.berkanterdogan.lab.springaoplab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAopLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopLabApplication.class, args);
+    }
+
+}
